b9fc495d085163909d53d21510dccfccfedb47dc
Weihao.Liu <hitliuweihao@gmail.com>
2023-04-08 17:03:00 +0800

    Backup the yellow computer

--- data_sender.py
+++ data_sender.py
@@ -5,11 +5,20 @@ from pathlib import Path
 from utils.udp_req import udp_send, udp_server
 import optparse
 
+<<<<<<< HEAD
 TRAIN_DATA = 'CMAPSSData/train_FD001.txt'
 
 def get_options():
     optParse = optparse.OptionParser()
     optParse.add_option("-p","--port",default="10650",type=int,help="sent to port")
+=======
+# TRAIN_DATA = 'train_data.txt'
+TRAIN_DATA = 'CMAPSSData/train_FD004.txt'
+
+def get_options():
+    optParse = optparse.OptionParser()
+    optParse.add_option("-p","--port",default="4700",type=int,help="sent to port")
+>>>>>>> Backup the yellow computer
     options, args = optParse.parse_args()
     return options
 
@@ -25,7 +34,11 @@ def start_sending():
         if tmp_msg.decode('utf-8').split(' ',1)[0] != buf_msg.decode('utf-8').split(' ',1)[0]:
             print(f'Machine {i} stop')
             i += 1
+<<<<<<< HEAD
             time.sleep(90)
+=======
+            # time.sleep(400)
+>>>>>>> Backup the yellow computer
             print(f'Machine {i} start')
 
         buf_msg = tmp_msg
@@ -33,4 +46,8 @@ def start_sending():
         time.sleep(0.005)
 
 if __name__ == '__main__':
+<<<<<<< HEAD
     start_sending()
+=======
+    start_sending()
+>>>>>>> Backup the yellow computer
